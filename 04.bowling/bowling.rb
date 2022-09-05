2aca84f03bccfe0e6f3752ff670dd0e9c18fd091
Sea Kimhour <seakimhour@gmail.com>
2022-09-05 18:54:10 +0900

    better name

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -5,37 +5,37 @@ input = ARGV[0].to_s.split(',')
 
 frames = 1
 total_score = 0
-sum = 0
-n = 0
+frame_score = 0
+frame_shot_num = 0
 
 def get_score(score)
   score == 'X' ? 10 : score.to_i
 end
 
 input.each_with_index do |score, index|
-  sum += get_score(score)
-  n += 1
+  frame_score += get_score(score)
+  frame_shot_num += 1
 
   if frames < 10
     if score == 'X'
-      total_score += (sum + get_score(input[index + 1]) + get_score(input[index + 2]))
-      sum = 0
-      n = 0
+      total_score += (frame_score + get_score(input[index + 1]) + get_score(input[index + 2]))
+      frame_score = 0
+      frame_shot_num = 0
       frames += 1
-    elsif sum == 10
-      total_score += (sum + get_score(input[index + 1]))
-      sum = 0
-      n = 0
+    elsif frame_score == 10
+      total_score += (frame_score + get_score(input[index + 1]))
+      frame_score = 0
+      frame_shot_num = 0
       frames += 1
-    elsif n == 2
-      total_score += sum
-      sum = 0
-      n = 0
+    elsif frame_shot_num == 2
+      total_score += frame_score
+      frame_score = 0
+      frame_shot_num = 0
       frames += 1
     end
   else
-    total_score += sum
-    sum = 0
+    total_score += frame_score
+    frame_score = 0
   end
 end
 

15caeac6f06aafda25340387f41fe3ab961f0a8e
Sea Kimhour <seakimhour@gmail.com>
2022-09-05 11:21:11 +0900

    rubocopチェックの修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'optparse'
 require 'date'
@@ -11,17 +12,17 @@ month = input['m'] ? input['m'].to_i : now.month
 this_month = now.year == year && now.month == month
 
 current_day_of_week = Date.new(year, month).wday
-puts '     ' + month.to_s + '月　' + year.to_s
+puts "     #{month}月　#{year}"
 puts '日 月 火 水 木 金 土'
-print "   " * current_day_of_week
+print '   ' * current_day_of_week
 
 dates_in_month = Date.new(year, month, -1).day
 
 (1..dates_in_month).each do |date|
   if this_month && now.day == date
-    print "\e[32m#{date.to_s.rjust(2) + ' '}\e[0m"
-  else 
-    print date.to_s.rjust(2) + ' '
+    print "\e[32m#{date.to_s.rjust(2)} \e[0m"
+  else
+    print "#{date.to_s.rjust(2)} "
   end
 
   if current_day_of_week < 6

99830268d3bf25d2e88542c23fc246f215b2170a
Sea Kimhour <seakimhour@gmail.com>
2022-09-28 16:56:11 +0900

    lsコマンドを作る4

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
 def get_string_space(string, width)
   string = string.force_encoding(Encoding::UTF_8) if string != ''
@@ -12,11 +13,39 @@ def get_string_space(string, width)
   string + (' ' * (width - string_width))
 end
 
-cols = 3
-col_width = 30
+def readable_permisions(file_name)
+  permissions = {
+    '7' => 'rwx',
+    '6' => 'rw-',
+    '5' => 'r-x',
+    '4' => 'r--',
+    '3' => '-wx',
+    '2' => '-w-',
+    '1' => '--x'
+  }
+
+  permisions_string = File.directory?(file_name) ? 'd' : '-'
+  format('%o', File.stat(file_name).mode)[-3..].chars.each do |digit|
+    permisions_string += permissions[digit]
+  end
+  permisions_string
+end
+
+def item_long_format(path, item)
+  file_name = "#{path}/#{item}"
+
+  file = File.stat(file_name)
+
+  readable_permisions(file_name) + format(' %3s', file.nlink) + format(' %15s', Etc.getpwuid(file.uid).name) + format(' %15s', Etc.getgrgid(file.gid).name) + format(' %8s', file.size) + format(' %s', file.ctime.ctime) + format(' %s', item)
+end
+
 options = {
+  path: '.',
+  cols: 3,
+  col_width: 30,
   hidden_files: 0,
-  reverse: false
+  reverse: false,
+  long_format: false
 }
 
 parser = OptionParser.new
@@ -26,16 +55,26 @@ end
 parser.on('-r', 'Reverse the sorting order') do
   options[:reverse] = true
 end
+parser.on('-l', 'List files in long format') do
+  options[:long_format] = true
+  options[:cols] = 1
+end
 parser.parse!
 
-items = Dir.glob('*', options[:hidden_files], base: ARGV[0] && Dir.exist?(ARGV[0]) ? ARGV[0] : '.').sort
+# Change path if argv and directory exist
+options[:path] = ARGV[0] if ARGV[0] && Dir.exist?(ARGV[0])
+
+items = Dir.glob('*', options[:hidden_files], base: options[:path]).sort
 items.reverse! if options[:reverse]
 
-rows = (items.count / cols.to_f).ceil
-
+rows = (items.count / options[:cols].to_f).ceil
 rows.times do |r|
-  cols.times do |c|
-    printf(get_string_space(items[r + (c * rows)].to_s, col_width))
+  options[:cols].times do |c|
+    if options[:long_format]
+      printf(item_long_format(options[:path], items[r]))
+    else
+      printf(get_string_space(items[r + (c * rows)].to_s, options[:col_width]))
+    end
   end
   puts
 end

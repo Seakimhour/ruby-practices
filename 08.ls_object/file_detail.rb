ba0bc6e937e9a4fe720ba3b7501330d3df223e1c
Sea Kimhour <seakimhour@gmail.com>
2022-11-09 16:21:10 +0900

    FileDetail 追加

---
+++ 08.ls_object/file_detail.rb
@@ -0,0 +1,52 @@
+class FileDetail
+  require 'etc'
+
+  attr_reader :stat
+
+  COLUMN_WIDTH = 30
+
+  FILE_PERMISSION = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.freeze
+
+  def initialize(directory, name)
+    @name = name
+    @file_path = directory + name
+    @stat = detail_stat
+  end
+
+  def name_with_padding
+    name = @name.force_encoding(Encoding::UTF_8)
+    name_without_half_width_katakana = name.gsub(/[ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｧｨｩｪｫｬｭｮﾞﾟ]/u, '*')
+    name_width = name_without_half_width_katakana.chars.map { |char| char.bytesize == 1 ? 1 : 2 }.sum
+    name + ' ' * (COLUMN_WIDTH - name_width)
+  end
+
+  def permission
+    mode = format('%o', File.stat(@file_path).mode)
+    path_type = File.directory?(@file_path) ? 'd' : '-'
+    path_type + mode[-3..].chars.map { |digit| FILE_PERMISSION[digit] }.join
+  end
+
+  def detail_stat
+    file_stat = File.stat(@file_path)
+
+    {
+      blocks: file_stat.blocks,
+      permission: permission,
+      nlink: file_stat.nlink,
+      username: Etc.getpwuid(file_stat.uid).name,
+      groupname: Etc.getgrgid(file_stat.gid).name,
+      bytesize: file_stat.size,
+      time: file_stat.ctime.strftime('%b %e %k:%M'),
+      filename: @name
+    }
+  end
+end
--- 08.ls_object/printer.rb
+++ 08.ls_object/printer.rb
@@ -1,26 +1,17 @@
 # frozen_string_literal: true
 
 class Printer
-  require 'etc'
-
-  FILE_PERMISSION = {
-    '0' => '---',
-    '1' => '--x',
-    '2' => '-w-',
-    '3' => '-wx',
-    '4' => 'r--',
-    '5' => 'r-x',
-    '6' => 'rw-',
-    '7' => 'rwx'
-  }.freeze
+  require_relative 'file_detail'
 
   NUMBER_OF_COLUMN = 3
-  COLUMN_WIDTH = 30
 
   def initialize(directory, long_format, files)
-    @directory = directory
     @long_format = long_format
-    @files = files
+    @files_detail = files_detail_array(directory, files)
+  end
+
+  def files_detail_array(directory, files)
+    files.map { |name| FileDetail.new(directory, name) }
   end
 
   def print
@@ -28,24 +19,25 @@ class Printer
   end
 
   def short_format
-    number_of_row = (@files.count / NUMBER_OF_COLUMN.to_f).ceil
+    number_of_row = (@files_detail.count / NUMBER_OF_COLUMN.to_f).ceil
+
     number_of_row.times do |row|
       NUMBER_OF_COLUMN.times do |column|
-        printf(get_name_padding(@files[row + column * number_of_row].to_s))
+        index = row + column * number_of_row
+        printf(@files_detail[index].name_with_padding) if @files_detail[index]
       end
       puts
     end
   end
 
   def detail_format
-    files_detail = @files.map { |file| file_detail_format(file) }
-    total_block = files_detail.map { |file| file[:blocks] }.sum
-    columns_length = find_columns_length(files_detail)
+    total_block = @files_detail.map { |file| file.stat[:blocks] }.sum
+    columns_length = find_columns_length(@files_detail)
 
     # ls use --block-size=1024 while File::Stat Blocks use 512B make it return double the size compared to ls
     puts "total #{total_block / 2}"
 
-    files_detail.each do |stat|
+    @files_detail.each do |file|
       printf([
         '%<permission>s',
         "%<nlink>#{columns_length[:nlink]}i",
@@ -54,47 +46,17 @@ class Printer
         "%<bytesize>#{columns_length[:bytesize]}s",
         '%<time>s',
         '%<filename>s'
-      ].join(' '), stat)
+      ].join(' '), file.stat)
       puts
     end
   end
 
-  def get_name_padding(string)
-    string = string.force_encoding(Encoding::UTF_8) if string != ''
-    string_without_half_width_katakana = string.gsub(/[ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｧｨｩｪｫｬｭｮﾞﾟ]/u, '*')
-    string_width = string_without_half_width_katakana.chars.map { |char| char.bytesize == 1 ? 1 : 2 }.sum
-    string + ' ' * (COLUMN_WIDTH - string_width)
-  end
-
-  def readable_permission(file_path)
-    mode = format('%o', File.stat(file_path).mode)
-    path_type = File.directory?(file_path) ? 'd' : '-'
-    path_type + mode[-3..].chars.map { |digit| FILE_PERMISSION[digit] }.join
-  end
-
   def find_columns_length(files_detail)
     {
-      nlink: files_detail.map { |stat| stat[:nlink].to_s.length }.max,
-      username: files_detail.map { |stat| stat[:username].length }.max,
-      groupname: files_detail.map { |stat| stat[:groupname].length }.max,
-      bytesize: files_detail.map { |stat| stat[:bytesize].to_s.length }.max
-    }
-  end
-
-  def file_detail_format(file)
-    file_path = @directory + file
-
-    file_stat = File.stat(file_path)
-
-    {
-      blocks: file_stat.blocks,
-      permission: readable_permission(file_path),
-      nlink: file_stat.nlink,
-      username: Etc.getpwuid(file_stat.uid).name,
-      groupname: Etc.getgrgid(file_stat.gid).name,
-      bytesize: file_stat.size,
-      time: file_stat.ctime.strftime('%b %e %k:%M'),
-      filename: file
+      nlink: files_detail.map { |file| file.stat[:nlink].to_s.length }.max,
+      username: files_detail.map { |file| file.stat[:username].length }.max,
+      groupname: files_detail.map { |file| file.stat[:groupname].length }.max,
+      bytesize: files_detail.map { |file| file.stat[:bytesize].to_s.length }.max
     }
   end
 end
